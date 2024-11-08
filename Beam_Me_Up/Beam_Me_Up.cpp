13ea14efd5f0eec80f9784dd9f323d6565bbccea
lonaasan <shadowlona@shadowlona.dev>
2024-11-09 00:03:28 +0100

    Initial commit

---
+++ Beam_Me_Up/Beam_Me_Up.cpp
@@ -0,0 +1,12 @@
+﻿// Beam_Me_Up.cpp : Defines the entry point for the application.
+//
+
+#include "Beam_Me_Up.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
